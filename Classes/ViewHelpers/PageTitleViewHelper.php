bd23f6c69c9d762cd6a9da116dc43acf0352b96e
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2015-03-23 17:26:38 +0100

    [TASK] WWW-1997 Show page title on detail view

---
+++ Classes/ViewHelpers/PageTitleViewHelper.php
@@ -0,0 +1,51 @@
+<?php
+namespace Subugoe\Nkwkeywords\ViewHelpers;
+
+/* * *************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2015 Ingo Pfennigstorf <pfennigstorf@sub-goettingen.de>
+ *      Goettingen State Library
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ * ************************************************************* */
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3\CMS\Frontend\Page\PageRepository;
+
+/**
+ * View helper for returning the page title of a pid
+ */
+class PageTitleViewHelper extends AbstractViewHelper {
+
+	/**
+	 * @param int $pid
+	 * @return string
+	 */
+	public function render($pid) {
+
+		/** @var \TYPO3\CMS\Frontend\Page\PageRepository $pageRepository */
+		$pageRepository = $this->objectManager->get(\TYPO3\CMS\Frontend\Page\PageRepository::class);
+
+		$page = $pageRepository->getPage($pid);
+		$title = $page['title'];
+
+		return $title;
+
+	}
+}
